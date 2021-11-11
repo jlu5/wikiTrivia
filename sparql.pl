6c8af661189152c8a6c264fb74dbf85945a31959
James Lu <james@overdrivenetworks.com>
2021-11-10 21:47:13 -0800

    Parse rows into their individual pieces

--- sparql.pl
+++ sparql.pl
@@ -9,3 +9,16 @@ get_data(Row) :-
                  host('query.wikidata.org'),
                  path('/sparql')
                ]).
+
+% Parse a Q&A Row into its constituent components, where rows are in the form:
+% Row = row('http://www.wikidata.org/entity/Q1930',
+%           literal(lang(en, 'Ottawa')),
+%           'http://www.wikidata.org/entity/Q16',
+%           literal(lang(en, 'Canada')),
+%           literal(lang(en, 'CA, ca, can, CAN, British North America, Can., Dominion of Canada')))
+% Each row item corresponds to a variable (?abcd) selected in the SPARQL query, with the expected
+% order as shown.
+parse_row(Row, LHSNode, LHSLabel, RHSNode, RHSLabel, RHSAltLabels) :-
+  Row = row(LHSNode, literal(lang(_, LHSLabel)), RHSNode, literal(lang(_, RHSLabel)),
+            % Alt labels are split by ","
+            literal(lang(_, RHSAltLabelsRaw))), split_string(RHSAltLabelsRaw, ",", " ", RHSAltLabels).
