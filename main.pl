d78eb7a5f0d753772f992fccb6c54e056a629ff1
Morgan Mo <momorgan@student.ubc.ca>
2021-11-12 13:49:35 -0800

    Added asks_questions

--- main.pl
+++ main.pl
@@ -5,16 +5,16 @@
 num_questions(5).
 
 known_topics([
-  quiz_topic("queries/capitals-to-countries.rq", "Capital cities of various countries"),
-  quiz_topic("queries/1000-vital-wikipedia-topics.rq", "1000 Vital Wikipedia topics (general Q&A)"),
-  quiz_topic("queries/billion-euro-companies.rq", "Billion euro companies... and where they're from")
+  quiz_topic("queries/capitals-to-countries.rq", "Capital cities of various countries", "What country is ~w the capital of?"),
+  quiz_topic("queries/1000-vital-wikipedia-topics.rq", "1000 Vital Wikipedia topics (general Q&A)", "~w?"),
+  quiz_topic("queries/billion-euro-companies.rq", "Billion euro companies... and where they're from", "What country is ~w based in?")
   % Enable these once we have a more lenient way of scoring numerical answers!
   %quiz_topic("queries/inception-vancouver-companies.rq", "Vancouver-based companies and when they were founded"),
   %quiz_topic("queries/inception-oses-programming-langs.rq", "Birth dates of programming langs and operating systems")
 ]).
 
 print_known_topics([], _).
-print_known_topics([quiz_topic(_Filename, TopicName)|Rest], CurrentIndex) :-
+print_known_topics([quiz_topic(_Filename, TopicName, _FormatString)|Rest], CurrentIndex) :-
   write(CurrentIndex), write(". "), writeln(TopicName),
   NewIndex is CurrentIndex + 1, print_known_topics(Rest, NewIndex).
 
@@ -28,20 +28,37 @@ choose_topic(QuizTopic) :-
   writeln("Invalid input"),
   choose_topic(QuizTopic).
 
+score_answer(UserAnswer, RHSLabel, 1) :-
+  string_lower(UserAnswer, LowerUserAnswer),
+  string_lower(RHSLabel, LowerRHSLabel),
+  ==(LowerUserAnswer, LowerRHSLabel),
+  format("Correct! The answer is ~w \n", [RHSLabel]).
+score_answer(UserAnswer, RHSLabel, 0) :-
+  format("Incorrect! The answer is ~w \n", [RHSLabel]).
+
 % ask_questions will take in a Row, prompt the user with the question, and verify the response. It will recurse with further questions until RemainingQuestions becomes 0
-ask_questions(_, 0).
-ask_questions(AllRows, RemainingQuestions) :- 
+ask_questions(_, _, 0, Score) :-
+  writeln(Score).
+ask_questions(AllRows, FormatString, RemainingQuestions, CurrentScore) :- 
   random_member(Row, AllRows),
   parse_row(Row, LHSNode, LHSLabel, RHSNode, RHSLabel, RHSAltLabels),
-  write(LHSLabel),
-  write(RHSLabel).
+  format(atom(Question), FormatString, [LHSLabel]),
+  writeln(""),
+  writeln(Question),
+  read_line_to_string(user_input, UserAnswer),
+  % Score is 0 or 1
+  score_answer(UserAnswer, RHSLabel, Score),
+  NewScore is CurrentScore + Score,
+  format("Your current score is: ~d \n", [NewScore]),
+  NewRemainingQuestions is RemainingQuestions - 1,
+  ask_questions(AllRows, FormatString, NewRemainingQuestions, NewScore).
 
-play_topic(quiz_topic(TopicFilename, TopicDescription)) :-
-  writeln(format("Playing topic: ~w", [TopicDescription])),
+play_topic(quiz_topic(TopicFilename, TopicDescription, FormatString)) :-
+  format("Playing topic: ~w", [TopicDescription]),
   get_all_from_rq_file(TopicFilename, AllRows),
 
   num_questions(RemainingQuestions),
-  ask_questions(AllRows, RemainingQuestions).
+  ask_questions(AllRows, FormatString, RemainingQuestions, 0).
 
 main(_) :-
   choose_topic(QuizTopic), !,
