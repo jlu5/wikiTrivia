922bb7dbb6e53a7ff2fb5087987322152c4b1221
James Lu <james@overdrivenetworks.com>
2021-11-13 01:38:50 -0800

    Accept alternative answers (from RHSAltLabels)

--- main.pl
+++ main.pl
@@ -28,34 +28,37 @@ choose_topic(QuizTopic) :-
   writeln("Invalid input"),
   choose_topic(QuizTopic).
 
-score_answer(UserAnswer, RHSLabel, 1) :-
-  string_lower(UserAnswer, LowerUserAnswer),
-  string_lower(RHSLabel, LowerRHSLabel),
-  ==(LowerUserAnswer, LowerRHSLabel),
-  format("Correct! The answer is ~w \n", [RHSLabel]).
-score_answer(UserAnswer, RHSLabel, 0) :-
-  format("Incorrect! The answer is ~w \n", [RHSLabel]).
+score_answer(UserAnswer, CanonicalAnswer, AlternativeAnswers, 1) :-
+  AcceptableAnswers = [CanonicalAnswer|AlternativeAnswers],
+  % First convert input and accepted answers to lowercase so that matches are case insensitive
+  string_lower(UserAnswer, NormalizedUserAnswer),
+  maplist(string_lower, AcceptableAnswers, NormalizedAcceptableAnswers),
+  member(NormalizedUserAnswer, NormalizedAcceptableAnswers),
+  format("Correct! The (canonical) answer is ~w \n", [CanonicalAnswer]).
+score_answer(_, CanonicalAnswer, _, 0) :-
+  format("Incorrect! The answer is ~w \n", [CanonicalAnswer]).
 
 % ask_questions will take in a Row, prompt the user with the question, and verify the response. It will recurse with further questions until RemainingQuestions becomes 0
 ask_questions(_, _, 0, Score) :-
   format("Your final score is: ~d!\n", [Score]).
 ask_questions(AllRows, FormatString, RemainingQuestions, CurrentScore) :-
   random_member(Row, AllRows),
-  parse_row(Row, LHSNode, LHSLabel, RHSNode, RHSLabel, RHSAltLabels),
+  parse_row(Row, _, LHSLabel, _, RHSLabel, RHSAltLabels),
   format(atom(Question), FormatString, [LHSLabel]),
   writeln(""),
   writeln(Question),
   read_line_to_string(user_input, UserAnswer),
   % Score is 0 or 1
-  score_answer(UserAnswer, RHSLabel, Score),
+  score_answer(UserAnswer, RHSLabel, RHSAltLabels, Score),
   NewScore is CurrentScore + Score,
   format("Your current score is: ~d \n", [NewScore]),
   NewRemainingQuestions is RemainingQuestions - 1,
   ask_questions(AllRows, FormatString, NewRemainingQuestions, NewScore).
 
 play_topic(quiz_topic(TopicFilename, TopicDescription, FormatString)) :-
-  format("Playing topic: ~w", [TopicDescription]),
+  writeln("Loading questions from Wikidata..."),
   get_all_from_rq_file(TopicFilename, AllRows),
+  format("Playing topic: ~w", [TopicDescription]),
 
   num_questions(RemainingQuestions),
   ask_questions(AllRows, FormatString, RemainingQuestions, 0).
