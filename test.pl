2a7bb4b3bd7365da092048106b06aba65e814993
James Lu <james@overdrivenetworks.com>
2021-11-13 15:15:40 -0800

    tests for other row parsers

--- test.pl
+++ test.pl
@@ -11,6 +11,45 @@
 :- ensure_loaded("sparql.pl").
 :- ensure_loaded("main.pl").
 
+:- begin_tests('parse_question_label').
+
+test('parse_question_label success - translated literal', [nondet]) :-
+  parse_question_label(literal(lang(en, "Hello world")), "Hello world").
+test('parse_question_label fail - bare literal', [fail]) :-
+  parse_question_label(literal(1234), _).
+test('parse_question_label fail - mismatch', [fail]) :-
+  parse_question_label(literal(lang(en, "Hello world")), "dlrow olleH").
+
+:- end_tests('parse_question_label').
+
+:- begin_tests('parse_answer_label').
+
+test('parse_answer_label success - translated literal', [nondet]) :-
+  parse_answer_label(literal(lang(en, "Hello world")), "Hello world").
+test('parse_answer_label success - bare literal', [nondet]) :-
+  parse_answer_label(literal('2000-01-01T00:00:00Z'), '2000-01-01T00:00:00Z').
+test('parse_answer_label fail - output mismatch', [fail]) :-
+  parse_answer_label(literal(lang(en, "Hello world")), "dlrow olleH").
+test('parse_answer_label fail - invalid input', [fail]) :-
+  parse_answer_label(notAliteral(lang(abcdef, "312312312")), _).
+
+:- end_tests('parse_answer_label').
+
+:- begin_tests('parse_label_list').
+
+test('parse_label_list success - single item', [nondet]) :-
+  parse_label_list(literal(lang(en, "Hello world")), ["Hello world"]).
+test('parse_label_list success - multiple items', [nondet]) :-
+  parse_label_list(literal(lang(en, "A, B, C d e, f")), ["A", "B", "C d e", "f"]).
+test('parse_label_list success - null', [nondet]) :-
+  parse_label_list('$null$', []).
+test('parse_label_list fail - bare literal', [fail]) :-
+  parse_label_list(literal('2000-01-01T00:00:00Z'), _).
+test('parse_label_list fail - mismatched output', [fail]) :-
+  parse_label_list(literal(lang(en, "A, B, C d e, f g")), []).
+
+:- end_tests('parse_label_list').
+
 :- begin_tests('parse_row').
 
 test('parse_row success - with alt labels', [nondet]) :-
